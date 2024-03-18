2b113c09fb1a36ee2fee68107d8017cf8a7f8ef3
Dhiego Ramos <dhiegorp@gmail.com>
2024-03-18 18:46:12 -0300

    implementing very important tests

--- src/cli.rs
+++ src/cli.rs
@@ -1,5 +1,6 @@
 use clap::{Parser, Subcommand};
 use std::path::PathBuf;
+use rand::{thread_rng, Rng};
 
 
 pub const PHRASES: [&'static str; 8] = [
@@ -12,6 +13,11 @@ pub const PHRASES: [&'static str; 8] = [
     "Scwewy Wabbit!", 
     "wascawwy wabbit!"
 ];
+
+pub fn get_random_phrase() -> &'static str {
+    let random_idx = thread_rng().gen_range(0..PHRASES.len());
+    return PHRASES[random_idx];
+}
 
 
 #[derive(Subcommand, Debug)]
@@ -62,3 +68,31 @@ pub struct FuddConnect {
     #[command(subcommand)]
     subcommand: Option<FuddCommands>,
 }
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use std::collections::HashMap;
+
+    fn phrases_as_hashmap() -> HashMap<String, String> {
+         
+        return PHRASES.iter().map(|x| (x.to_string(), x.to_string()) ).collect::<HashMap<String, String>>();
+    }
+
+
+
+    #[test]
+    fn test_get_random_phrase_always_returns_a_valid_phrase() { //ultra important test
+        let phrases_map: HashMap<String, String> = phrases_as_hashmap();
+        for _ in 0..15 {
+            let a_phrase = phrases_map.get(get_random_phrase());
+            assert!(a_phrase.is_some())
+        }
+        
+    }
+
+}
+
+
+
