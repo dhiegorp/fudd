cec7c36844594edb5b776eb9bf0ebcefcc60406d
dhiegorp <dhiegorp@gmail.com>
2024-03-19 20:46:41 -0300

    using amiquip as amqp client

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ mod cli;
 use crate::cli::{FuddConnect, FuddCommands};
 use clap::Parser;
 use std::path::PathBuf;
+use std::error::Error;
+use std::fs::{metadata};
 
 fn get_messages(queue: &str, message_qt: &usize, output:&PathBuf, ack: &bool) {
     dbg!(queue);
@@ -21,6 +23,21 @@ fn publish_messages(exchange: &str, routing_key: &str, input:&PathBuf) {
     dbg!(routing_key);
     dbg!(input);
 }
+
+fn list_inputs(path: &PathBuf) -> Result<(), Box<dyn Error>> {
+
+    let meta = metadata(&path)?;    
+
+    if meta.is_file() {
+        //reads for a file    
+        todo!();
+    } else if meta.is_dir() {
+        //reads for a directory
+        todo!();
+    }    
+    Ok(())
+}
+
 
 pub fn main() {
     
