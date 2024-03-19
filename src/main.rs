cd4722dc3b16ddd2030a807173c52119891ea85c
dhiegorp <dhiegorp@gmail.com>
2024-03-19 17:23:16 -0300

    including dependencies and creating functions with debugging info

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,26 @@ fn get_messages(queue: &str, message_qt: &usize, output:&PathBuf, ack: &bool) {
     dbg!(ack);
 }
 
+fn consume_messages(queue: &str, output: &PathBuf, ack: &bool) {
+    dbg!(queue);
+    dbg!(output);
+    dbg!(ack);
+}
+
+fn publish_messages(exchange: &str, routing_key: &str, input:&PathBuf) {
+    dbg!(exchange);
+    dbg!(routing_key);
+    dbg!(input);
+}
+
 pub fn main() {
     
     let parsed_args = FuddConnect::parse();
     
     match &parsed_args.subcommand {
         FuddCommands::Get{queue, message_qt, output, ack} =>  get_messages(queue, message_qt, output, ack),
-        FuddCommands::Consume{..}=> todo!(),
-        FuddCommands::Publish{..} => todo!(),
+        FuddCommands::Consume{queue, output, ack}=> consume_messages(queue, output, ack),
+        FuddCommands::Publish{exchange, routing_key, input} => publish_messages(exchange, routing_key, input),
     }
 
 
